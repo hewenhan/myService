33427123a99a954a58fe7a37a6fc7dedbb682f2c
Blind Holmes <hewenhan@gmail.com>
2018-07-28 16:48:47 +0800

    update

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

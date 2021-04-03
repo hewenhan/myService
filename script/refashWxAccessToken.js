779444ae4fe375435ffa98642575af33d6f736b3
bh <hewenhan@gmail.com>
2021-04-03 18:38:30 +0800

    add jsTicket refrash

--- script/refashWxAccessToken.js
+++ script/refashWxAccessToken.js
@@ -2,8 +2,32 @@ var reqHttp = require("request_http");
 var __config = require("../config/config");
 var redis = require('redis-pool-fns')(__config.redis);
 
+var saveJsApiTicket = function (jsApiTicket) {
+	redis.set(`jsApiTicket_${__config.wechat.appId}`, jsApiTicket);
+};
+
 var saveAccessToken = function (accessToken) {
 	redis.set(`accessToken_${__config.wechat.appId}`, accessToken);
+};
+
+var getJsApiTicket = function (accessToken) {
+	var options = {
+		url: "https://api.weixin.qq.com/cgi-bin/ticket/getticket",
+		data: {
+			type: "jsapi",
+			access_token: accessToken
+		}
+	};
+
+	reqHttp(options, function (err, jsApiTicketObj) {
+		if (err) {
+			console.log(`${new Date()}: GET JSAPI_TICKET ERROR`);
+			return;
+		}
+
+		console.log(`GEN ${__config.wechat.appId} JSAPI_TICKET SUCCESS: ${jsApiTicketObj.ticket}`);
+		saveJsApiTicket(jsApiTicketObj.ticket);
+	});
 };
 
 var getAccessToken = function () {
@@ -25,6 +49,7 @@ var getAccessToken = function () {
 		console.log(`GEN ${__config.wechat.appId} ACCESS_TOKEN SUCCESS: ${accessTokenObj.access_token}`);
 
 		saveAccessToken(accessTokenObj.access_token);
+		getJsApiTicket(accessTokenObj.access_token);
 		setTimeout(getAccessToken, accessTokenObj.expires_in * 0.8 * 1000);
 	});
 };

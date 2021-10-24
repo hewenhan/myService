9fb5a298ec7769dade75cd6209fad3dfad6c01d3
Blind Holmes <hewenhan@gmail>
2021-10-24 16:41:17 +0800

    update

--- routes/api/all/cdnAuth.js
+++ routes/api/all/cdnAuth.js
@@ -1,3 +1,8 @@
 module.exports = (req, res, next) => {
-	res.success()
+	console.log("CDN AUTH HEADERS");
+	console.log(req.headers);
+
+	console.log("CDN AUTH PARAMS");
+	console.log(req.allParams);
+	res.success();
 };

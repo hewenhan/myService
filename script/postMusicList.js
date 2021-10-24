eb0d90d17a699fe33c32a128007b0c46c6a18610
Blind Holmes <hewenhan@gmail>
2021-10-24 16:37:58 +0800

    update

--- script/postMusicList.js
+++ script/postMusicList.js
@@ -21,24 +21,26 @@ var saveLog = function (msg) {
 };
 
 const playListArr = [
-['01', '地道战叙事曲 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/a5858ed0bdc8e1c2684cbf9525feff0608f62be3fd863040b66fdb3dae3736d1'],
-['02', '猪八戒背媳妇 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/ce7295a886b9add64a8d33566967efe47b481066422a16661a833341b50dfb1b'],
-['03', '秦腔牌子曲 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/ca3d57c8d63883e77e639c6e6eecad182b5e938d93fbed4c53256197e1149fe7'],
-['04', '大起板 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/4bae232844094994000cd987506f59283ae7ecf57ccd14779091a1325d6b544f'],
-['05', '舟山渔歌 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/ff1c7df4b0d373e4f6c9bfe804339d3f82c92bab394e7c43fe996d12c91875a8'],
-['06', '翻身道情 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/5aa4874f98f2d3f103670ebf947d28c77c0a61bbe9679004f2c38dba33ee3e88'],
-['07', '云雀 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/5d6576a018f8a85cfba388f610c12cd231f515f2976e4a9fe9fa40dbeb3fe67b'],
-['08', '山东小曲 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/ce84059d354c5d19127013e7f7310924ed023a0e71c974a2028fc30ba401aeb2'],
-['09', '红军哥哥回来了 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/bf1da323b0edd817bc01a3e2af13ed90ec98d4292d6982dad31f5711c75f48dc'],
-['10', '锄禾曲 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/67d57d9a57c83f07a306cb5c2f7e6ec0e252319ffbb5f2a372b5c4a1e648fbb5'],
-['11', '三个和尚 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/c74780e6c9d52ea137480e0ad9677cd4a24655c50e5867dccd7fe63b86733e85'],
-['12', '大姑娘美 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/8024051c69081cbf6edd2cc3dfb613aa4d8c45a8a491395d0ef495ddd83dfdfd'],
-['13', '翻身的日子 (板胡独奏)', '沈立良', 'https://static.hewenhan.me/userFiles/68136f0fcf60e84854442cd3c6c3c9e15a074f44eaeffd22903a7397b8a69b70']
+['01', 'Introduction', 'Magnavolt', 'https://static.hewenhan.me/userFiles/10a51003bcde9bfea0ba59b595fe801bac40aefde48adca7ca3c897d83460d57.mp3'],
+['02', 'City Ruins', 'Magnavolt', 'https://static.hewenhan.me/userFiles/e0f7416ac970efab53f44069159e4ac29bad3e9d8e458f6ada0324bb3e1e2d0f.mp3'],
+['03', 'Riot', 'Magnavolt', 'https://static.hewenhan.me/userFiles/ca8b177cef5ce3df7b85e08b1bd67856ca83295d9a971950410ffecb6e5400ed.mp3'],
+['04', 'Mirai', 'Magnavolt', 'https://static.hewenhan.me/userFiles/e10cd026ee96d541edad25ecae5662865d7cac115b936e59fc4ded84b98fd0aa.mp3'],
+['05', 'Seishin', 'Magnavolt', 'https://static.hewenhan.me/userFiles/cc5c0d3449ff384c99d9b808a2155c5ccc8f67eb6a4e2a39d753fe821c4875fb.mp3'],
+['06', 'The Club', 'Magnavolt', 'https://static.hewenhan.me/userFiles/13d16b982a2bece6ae31f97dbd69343c128aaeb6ac7d6812c17f2f4afc8d0fbd.mp3'],
+['07', 'Techno Orgasm', 'Magnavolt', 'https://static.hewenhan.me/userFiles/0426a8dcf0e029209be9514279998bd3dc8c2e963b7ac881dde38b2db07477c2.mp3'],
+['08', 'VIP Only', 'Magnavolt', 'https://static.hewenhan.me/userFiles/f4835c39ab1ad46c19323737785d19fce2d707b00bc2007cbb9f02381c6ec872.mp3'],
+['09', 'Rotten Flesh', 'Magnavolt', 'https://static.hewenhan.me/userFiles/3f99fb288c49676479387fd45b8f9d0acb3d7cd305ffbf1c950141b1266d2f98.mp3'],
+['10', 'Kronos Lab', 'Magnavolt', 'https://static.hewenhan.me/userFiles/b5e8c20f3abd28fbfa886d7f77b73b13cba44768f74ef750dfd8ab6d7955d3ca.mp3'],
+['11', 'Ero', 'Magnavolt', 'https://static.hewenhan.me/userFiles/750822539f5df99ce00dab3635a2c81896358642f5710e02c833d60326b0f30d.mp3'],
+['12', 'Painkillers', 'Magnavolt', 'https://static.hewenhan.me/userFiles/7f425f1c2e71a44032e13406e8dd55a4a1a0c11fa7cdb358e7562508cabd31ac.mp3'],
+['13', 'Faceless Man', 'Magnavolt', 'https://static.hewenhan.me/userFiles/b3e053f9ca2798d7b0548e04a25f70714964a2d0073d1eb654ace9ebcdaaf630.mp3'],
+['14', 'Till the end', 'Magnavolt', 'https://static.hewenhan.me/userFiles/e0063ea405b0fa3acd2d3161d7d89d4dfa28114167332c32afbf5299c3d1d863.mp3'],
+['15', 'Jiyuu', 'Magnavolt', 'https://static.hewenhan.me/userFiles/6e074ed817730ac89e8d6a9e6aa63dd1bdd84a327daa0abf374bb928aeb80ecb.mp3']
 ];
 
 const postConfig = {
-	artworkId: 890,
-	coverUrl: 'https://blog.hewenhan.me/wp-content/uploads/2021/04/cover-225x225.jpeg'
+	artworkId: 907,
+	coverUrl: 'https://blog.hewenhan.me/wp-content/uploads/2021/05/zoJhSQp1aqCnG_8b-300x300.jpg'
 };
 
 const buildTrackObj = (indexNum, title, artist, audioUrl) => {
@@ -61,9 +63,9 @@ const buildTrackObj = (indexNum, title, artist, audioUrl) => {
 };
 
 const trackData = {
-	'post_id': '889',
+	'post_id': '906',
 	tracks: [],
-	'nonce': '1377d5d950',
+	'nonce': '39d75080b1',
 	'action': 'cue_save_playlist_tracks'
 };
 
@@ -95,7 +97,7 @@ const postPlaylist = () => {
 			'Sec-Fetch-Dest': 'empty',
 			'Referer': 'https://blog.hewenhan.me/wp-admin/post.php?post=532&action=edit',
 			'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,hu;q=0.7,und;q=0.6,es;q=0.5',
-			'Cookie': 'wordpress_sec_d7f3078bf68ac0df50c4030b830c2a4f=hewenhan%7C1620414826%7CPClNShPM5Fe2PUDEAj01gKDhyr922zmi4S6iLpMR5rO%7C51ea2aa54e8ee0abfa7c598a0d0218a9524542b034580c2e9c70c5fcf6d06a67; Hm_lvt_880880fed08ec77c107a5711d9bfa1c9=1618164152,1618164343,1618164488,1618164506; wordpress_logged_in_d7f3078bf68ac0df50c4030b830c2a4f=hewenhan%7C1620414826%7CPClNShPM5Fe2PUDEAj01gKDhyr922zmi4S6iLpMR5rO%7C9abb006a513d6bcaac381cafa12f8cc63bc6450eb7c0af8609c765e73bf8c36f; PHPSESSID=3onqc74e0p7sceuvluvg8q6b92; Hm_lvt_37f30ee02580e891765c04f25597bc41=1619196696,1619205203,1619243014,1619287741; Hm_lpvt_37f30ee02580e891765c04f25597bc41=1619287741; wp-settings-time-1=1619287779; wp-settings-1=libraryContent%3Dbrowse%26editor%3Dhtml%26hidetb%3D0%26post_dfw%3Doff%26mfold%3Do%26posts_list_mode%3Dlist%26urlbutton%3Dnone'
+			'Cookie': 'wordpress_sec_d7f3078bf68ac0df50c4030b830c2a4f=hewenhan%7C1622102473%7CJALwm3U9Ez0GlOn4eFjeKdzGMbRo3e2qrGRVUuf7h91%7C95ca9e4854ad5e1ea80c89328849cdb3ab7de46a627e64d06d40a014cee97aa8; Hm_lvt_880880fed08ec77c107a5711d9bfa1c9=1618164152,1618164343,1618164488,1618164506; wordpress_logged_in_d7f3078bf68ac0df50c4030b830c2a4f=hewenhan%7C1622102473%7CJALwm3U9Ez0GlOn4eFjeKdzGMbRo3e2qrGRVUuf7h91%7C5347ec508d47b2422813e0e907e8c8766f362f4875f559a499cfce8dc72d266e; PHPSESSID=vrl6le6gsvcnjunpakn1m7c2d6; Hm_lvt_37f30ee02580e891765c04f25597bc41=1619745399,1620637908,1620892860,1621093902; Hm_lpvt_37f30ee02580e891765c04f25597bc41=1621093902; wp-settings-time-1=1621094176; wp-settings-1=libraryContent%3Dbrowse%26editor%3Dhtml%26hidetb%3D0%26post_dfw%3Doff%26mfold%3Do%26posts_list_mode%3Dlist%26urlbutton%3Dnone'
 		}
 	};
 

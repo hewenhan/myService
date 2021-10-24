48b76877f174b2182ce45d2a00cbf0dcbcc67c4b
Blind Holmes <hewenhan@gmail>
2021-10-24 16:54:16 +0800

    update

--- routes/api/all/cdnAuth.js
+++ routes/api/all/cdnAuth.js
@@ -4,5 +4,30 @@ module.exports = (req, res, next) => {
 
 	console.log("CDN AUTH PARAMS");
 	console.log(req.allParams);
-	res.success();
+
+	var ua = req.headers['user-agent'].trim().toLowerCase();
+
+	if (ua == "") {
+		res.status(404);
+		res.error("DON'T PLAY WITH ME!!");
+		return;
+	}
+	if (/linux/g.test(ua)) {
+		res.success();
+		return;
+	}
+	if (/chromium/g.test(ua)) {
+		res.success();
+		return;
+	}
+	if (/mozilla/g.test(ua)) {
+		res.success();
+		return;
+	}
+	if (/safari/g.test(ua)) {
+		res.success();
+		return;
+	}
+	res.status(404);
+	res.error("DON'T PLAY WITH ME!!");
 };

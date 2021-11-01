8dd2c173c21f6c946fbfd64f3c3d6c79f40c4087
Blind Holmes <hewenhan@gmail.com>
2021-11-01 13:16:50 +0800

    update

--- routes/api/post/userResourceUrlParse.js
+++ routes/api/post/userResourceUrlParse.js
@@ -247,10 +247,13 @@ var parseXimalayaResource = (req, res, cookie) => {
 	var options = {
 		url: req.allParams.urlParse.href,
 		headers: {
-			'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-			'Cookie': cookie
+			'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
 		}
 	};
+
+	if (cookie) {
+		options.headers.Cookie = cookie;
+	}
 	reqHttp(options, (err, data, resHeaders, resCode) => {
 		if (err) {
 			console.log(options);
